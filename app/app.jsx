dffc843c68360ff0fab142967dcace1f67c4377c
HienNguyen03 <lily31392@gmail.com>
2017-06-15 01:36:21 +0300

    add store

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,17 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hasHistory} = require('react-router');
 
 var TodoApp = require('TodoApp');
+
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(actions.addTodo('Take transcript'));
+store.dispatch(actions.setSearchText('transcript'));
+store.dispatch(actions.toggleShowCompleted());
 //Load foundation
 $(document).foundation();
 
